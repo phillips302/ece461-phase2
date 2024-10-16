35e07b418d67ce37a49348bfc49e6d66b20f2d8c
npeter <nilayahsp@gmail.com>
2024-10-16 16:09:13 -0400

    initialize a file to handle searching packages and finding matches

---
+++ src/searchPackages.ts
@@ -0,0 +1,8 @@
+// Search for a package using a regular expression over package names and READMEs. 
+// The results of a package search should resemble the “directory” view, but a subset thereof
+
+//iterate through each package
+//search the package name
+//search the READMEs
+
+//create a directory of the packages that match the search
