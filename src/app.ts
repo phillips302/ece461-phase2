eb9f7e8f51800f34d89d1928d90f4c54afa3736a
phillips302 <gphill1030@gmail.com>
2024-10-25 22:46:44 -0400

    testing query endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import homeRoutes from './routes/helloWorld.js';
 import screen1Routes from './routes/route1.js';
+import queryVersionRoutes from './routes/queryVersion.js';
 
 const app = express();
 const port = 80;
@@ -8,6 +9,7 @@ const port = 80;
 // Use imported routes ?
 app.use('/', homeRoutes);
 app.use('/', screen1Routes);
+app.use('/', queryVersionRoutes);
 
 app.listen(port, () => {
   console.log(`Express is listening exposed at port ${port}`);
