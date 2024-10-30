3b6abe28bcfb2d5d55b7a28e8f1b7373d6967868
rileythomas529 <thom1028@purdue.edu>
2024-10-30 15:31:23 -0400

    types from OpenAPI spec schemas

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,17 +1,58 @@
 // Define the schema for PackageQuery, EnumerateOffset, and PackageMetadata
 
-export interface PackageQuery {
-    name: string;
-    version?: string;
+  export interface PackageQuery {
+    Version?: string;
+    Name: string;
   }
   
   export interface EnumerateOffset {
     offset: number;
   }
+
+  export interface Package {
+    metadata: PackageMetadata;
+    data: PackageData;
+  }
   
   export interface PackageMetadata {
-    ID: string;
     Name: string;
+    ID: string;
     Version: string;
+  }
+
+  export interface PackageData {
+    content: string;
+    URL: string;
+    debloat: boolean;
+    JSProgram: string;
+  }
+
+  export interface PackageRating {
+    BusFactor: number;
+    BusFactorLatency: number;
+    Correctness: number;
+    CorrectnessLatency: number;
+    RampUp: number;
+    RampUpLatency: number;
+    ResponsiveMaintainer: number;
+    ResponsiveMaintainerLatency: number;
+    LicenseScore: number;
+    LicenseScoreLatency: number;
+    GoodPinningPractice: number;
+    GoodPinningPracticeLatency: number;
+    PullRequest: number;
+    PullRequestLatency: number;
+    NetScore: number;
+    NetScoreLatency: number;
+  }
+
+  export interface PackageCost {
+    ID: string;
+    standaloneCost: number;
+    totalCost: number;
+  }
+
+  export interface PackageByRegEx {
+    RegEx: string;
   }
   
