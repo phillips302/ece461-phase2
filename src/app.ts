c7d6a374a03267930a94d54bf4fa8ccdf911d46d
Garrett Phillips <89997931+phillips302@users.noreply.github.com>
2024-11-21 15:51:07 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const port = 8081;
 
 const corsOptions = {
   origin: 'https://prod.d1k3s8at0zz65i.amplifyapp.com',
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 430 //TEST; CHANGE BACK TO 200 IF NOT CASE
 };
 app.use(cors(corsOptions));
 
