34a422ae78d17bd5aca1a2984c704fa7785d707d
phillips302 <gphill1030@gmail.com>
2024-12-02 17:56:23 -0500

    changed cors back

--- src/app.ts
+++ src/app.ts
@@ -14,15 +14,15 @@ import bodyParser from 'body-parser';
 
 const app: Application = express();
 const port = 8081;
-
-app.use(cors());
 /*
+app.use(cors());
+*/
 const corsOptions = {
   origin: 'https://prod.d1k3s8at0zz65i.amplifyapp.com',
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-*/
+
 // Set a higher limit for the request body size
 app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as needed
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
