6b1ba02cdabe66a399d8968845b59146aa5fb86a
Garrett Phillips <89997931+phillips302@users.noreply.github.com>
2024-12-04 17:25:29 -0500

    Update testConnection.ts

--- src/rds/testConnection.ts
+++ src/rds/testConnection.ts
@@ -20,7 +20,7 @@ const client = new Client({
 export const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: 5432,
-    username: process.env.POSTGRES_USER,
+    user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
 });
